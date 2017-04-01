1118bb18be57a7dd70f18e996d47158474d0f2a9
gaoyangxiaozhu <1456543560@qq.com>
2017-04-01 22:03:50 +0800

    update sh

--- update_remote.sh
+++ update_remote.sh
@@ -6,9 +6,17 @@ _update_time="$(date '+%Y-%m-%d')"
 COMMIT_MSG="update htmls in ${_update_time}"
 
 git pull
+git submodule update --remote --merge
 git add --all
+
+set +e
+
 git commit -am "$COMMIT_MSG"
-git push
+
+# only push if have new files generated
+if [ "$?" -eq 0 ];then
+    git push
+fi
 
 exit 0
 
