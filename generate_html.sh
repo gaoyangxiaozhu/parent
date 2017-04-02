51b312442da8ba0c2b8e8ac097d517607d6d8f47
gaoyangxiaozhu <1456543560@qq.com>
2017-04-02 19:24:42 +0800

    modifu generate_html.sh

--- generate_html.sh
+++ generate_html.sh
@@ -1,31 +1,45 @@
 #!/bin/bash
 
-
-MD_SOURCE_DIR='child'
+MD_SOURCE_DIRS='child child2'
 HTML_DIR='docs'
+MD_FILES=
+
+declare -A MD_DIRTOFILE_ARR
 
 # get all md files from ${MD_SOURCE_DIR}
-MD_FILES=`ls ${MD_SOURCE_DIR} | grep -E '*.md$'`
+for MD_SOURCE_DIR in ${MD_SOURCE_DIRS}
+do
+    MD_FILES=`ls ${MD_SOURCE_DIR} | grep -E '*.md$'`
+    if [ -n "${MD_FILES}" ];then
+        MD_DIRTOFILE_ARR[${MD_SOURCE_DIR}]=${MD_FILES}
+    fi
+done
+
+# print all md file name
+for MD_SOURCE_DIR in ${!MD_DIRTOFILE_ARR[*]}
+do
+    for FILE_NAME in ${MD_DIRTOFILE_ARR[${MD_SOURCE_DIR}]}
+    do
+        echo ${MD_SOURCE_DIR}/${FILE_NAME}
+    done
+done
 
 set -e 
 
-# exit if no md files
-if [ -z "$MD_FILES" ];then 
-    exit 0
-fi
 
-for FILE in $MD_FILES
+for MD_SOURCE_DIR in ${!MD_DIRTOFILE_ARR[*]}
 do
-    HTML_FILE_NAME=`echo $FILE | sed 's/\.md$/\.html/g'`
-    pandoc ${MD_SOURCE_DIR}/${FILE} \
-    -f markdown \
-    -t html \
-    -s -o \
-    ${HTML_DIR}/${HTML_FILE_NAME}
+    for FILE_NAME in ${MD_DIRTOFILE_ARR[${MD_SOURCE_DIR}]}
+    do
+        HTML_FILE_NAME=`echo $FILE_NAME | sed 's/\.md$/\.html/g'`
+        pandoc ${MD_SOURCE_DIR}/${FILE_NAME} \
+        -f markdown \
+        -t html \
+        -s -o \
+        ${HTML_DIR}/${HTML_FILE_NAME}
+    done
 done
 
-exit 0
-
-    
+exit 0  
 
 
---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+a="child child2"
+
+for i in ${a}
+do 
+    echo $i
+done
+
